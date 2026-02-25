deb47d00effb4968ce869d8dd8d090672337787b
Keysi Quiñe <quinelindsay@gmail.com>
2026-02-25 07:33:14 -0500

    Delete TipoDatosCadena.py

--- TipoDatosCadena.py
+++
@@ -1,9 +0,0 @@
-#Ejercicios 01
-frase = "Python es genial"
-subcadena = frase[0:6] # Del índice 0 al 5
-print(f"Palabra extraída: {subcadena}")
-
-#Ejercicio 02
-clase = "Programación en Python"
-print(clase.upper())
-print(clase.count('a'))
